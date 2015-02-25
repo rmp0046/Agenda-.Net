31805181d15d5e1a55aa6a6fc79dc2fb73eb74a2
rmp0046 <rmp0046@alu.ubu.es>
2015-02-25 23:14:50 +0100

    Vesion Inicial del Modelo

---
+++ Modelo/Llamada.vb
@@ -0,0 +1,36 @@
+﻿Public Class Llamada
+
+    Private _ID As Integer
+    Private _Persona As Persona
+    Private _Fecha As Date
+
+    Public Property ID As Integer
+        Get
+            Return _ID
+        End Get
+        Set(value As Integer)
+            _ID = value
+        End Set
+    End Property
+
+    Public Property Persona As Persona
+        Get
+            Return _Persona
+        End Get
+        Set(value As Persona)
+            _Persona = value
+        End Set
+    End Property
+    Public Property Fecha As Date
+        Get
+            Return _Fecha
+        End Get
+        Set(value As Date)
+            _Fecha = value
+        End Set
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return ID.ToString + " " + Persona.ToString
+    End Function
+End Class
---
+++ Modelo/Persona.vb
@@ -0,0 +1,39 @@
+﻿Public Class Persona
+
+    Private _ID As Integer
+    Private _Nombre As String
+    Private _Ciudad As String
+    Private _Correo As String
+
+    Public Property ID As Integer
+        Get
+            Return _ID
+        End Get
+        Set(value As Integer)
+            _ID = value
+        End Set
+    End Property
+
+    Public Property Nombre As String
+        Get
+            Return _Nombre
+        End Get
+        Set(value As String)
+            _Nombre = value
+        End Set
+    End Property
+
+    Public Property Ciudad() As String
+        Get
+            Return _Ciudad
+        End Get
+        Set(value As String)
+            _Ciudad = value
+        End Set
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return ID.ToString + " " + Nombre
+    End Function
+
+End Class
